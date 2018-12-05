2280fc95f9eae97322b189f6fc26764a50942b1d
oarodriguez <omargumer22@gmail.com>
2018-12-05 10:44:05 -0430

    Vista del Formulario para la Carga de los Datos de la Imagen.

--- resources/views/imagenes/create.blade.php
+++ resources/views/imagenes/create.blade.php
@@ -4,16 +4,8 @@
 	
 @section('content')	
 
-	@if($errors->any())
-		<div class="alert alert-danger">
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-	@endif
-
+	@include('common.errors')
+	
 	{!! Form::open(['route'=>'imagenes.store', 'method'=>'POST', 'files'=>true]) !!}
 
 		@include('imagenes.form')
